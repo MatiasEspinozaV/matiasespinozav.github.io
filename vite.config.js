f7e0f149daf86cfd9ff9703bc7f494c8e1b18185
MatiasEspinozaV <matiasespinozav.lk@gmail.com>
2025-06-02 08:44:18 -0400

    Fix [02/09/25]

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  base: './dist/index.html',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.ts', '.tsx', '.jsx'],
